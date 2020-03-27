eaad76ba424544a691abf088374e60688a6efbc9
Matthew Kennedy <kennedymj97@gmail.com>
2020-03-27 17:50:22 +0000

    extraction of rows and columns, random new tiles working

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use std::fmt;
 
+use rand::Rng;
+
 fn main() {
+    let game1 = GameState::new();
+    println!("{}", game1);
     let game = GameState::from(81985529216486895);
     println!("{}", game);
 }
@@ -9,7 +13,7 @@ struct GameState(u64);
 
 impl GameState {
     fn new() -> GameState {
-        let game = GameState(0);
+        let mut game = GameState(0);
         game.generate_random_tile();
         game.generate_random_tile();
         game
@@ -37,12 +41,67 @@ impl GameState {
         ((self.0 >> ((15 - idx) * 4)) & 15) as u32
     }
 
+    fn extract_row(&self, row_num: u32) -> u32 {
+        ((self.0 >> ((3 - row_num) * 16)) & 65535) as u32
+    }
+
+    fn extract_col(&self, col_num: u32) -> u32 {
+        // extract the 4 cells
+        let mut tile_vals: [u32; 4] = (0..4).fold([0; 4], |mut arr, idx| {
+            let tile_val = self.extract_tile((col_num + (idx * 4)) as usize);
+            arr[idx as usize] = tile_val;
+            arr
+        });
+        // shift the cells appropriately
+        tile_vals[0] = tile_vals[0] << 12;
+        tile_vals[1] = tile_vals[1] << 8;
+        tile_vals[2] = tile_vals[2] << 4;
+
+        // or the shifted vals together for the 16 bit column value
+        tile_vals[0] | tile_vals[1] | tile_vals[2] | tile_vals[3]
+    }
+
     /// Function to generate random tiles
     /// The random tiles are either 2 or 4
     /// There is a 90% chance the tile generated is a 2 and 10% of a 4
     /// At the start of the game 2 tiles are generated randomly and after a tile is generated after
     /// a move has been completed
-    fn generate_random_tile(&self) {}
+    fn generate_random_tile(&mut self) {
+        let zero_tiles = self.get_zero_tiles();
+        let mut rng = rand::thread_rng();
+        let rand_idx = rng.gen_range(0, zero_tiles.len());
+        let rand_val = if rng.gen_range(0, 10) < 9 { 1 } else { 2 };
+        self.0 = self.0 | (rand_val << ((15 - zero_tiles[rand_idx]) * 4));
+    }
+
+    fn get_zero_tiles(&self) -> Vec<usize> {
+        (0..16).fold(Vec::new(), |mut vec, idx| {
+            let tile_val = self.extract_tile(idx);
+            if tile_val == 0 {
+                vec.push(idx)
+            };
+            vec
+        })
+    }
+}
+
+// Temporary function just used to sanity check the bitwise operations
+fn to_binary(num: u64) -> String {
+    let mut temp = num;
+    let mut bits = String::new();
+    while temp > 0 {
+        if temp % 2 == 0 {
+            bits.push('0')
+        } else {
+            bits.push('1')
+        };
+
+        temp /= 2;
+    }
+    while bits.len() < 64 {
+        bits.push('0');
+    }
+    bits.chars().rev().collect::<String>()
 }
 
 impl fmt::Display for GameState {
@@ -93,4 +152,11 @@ fn format_val(val: &Option<u32>) -> String {
             x
         }
     }
+}
+
+enum Move {
+    Up,
+    Down,
+    Left,
+    Right,
 }
