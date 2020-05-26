c56598276d1b027823e66fe7a1b28bb10bab5d19
Matthew Kennedy <kennedymj97@gmail.com>
2020-05-26 09:24:45 +0100

    experimenting with wasm

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,13 @@
+use wasm_bindgen::prelude::*;
+
 pub mod ai;
 pub mod engine;
-pub mod ui;
+//pub mod ui;
 
 // https://dev.to/robertorojasr/rust-project-structure-example-step-by-step-3ee
 // ^^ good summary of how the module system works in rust.
+
+#[wasm_bindgen]
+pub fn mult10(num: i32) -> i32 {
+    num * 10
+}
--- src/ui.rs
+++ src/ui.rs
@@ -1,53 +1,53 @@
-use std::io::{stdin, stdout, Write};
-use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
-
-use crate::engine as GameEngine;
-
-pub fn start_game_in_ui() {
-    let mut board = GameEngine::new_game();
-    let stdin = stdin();
-
-    let mut stdout = stdout().into_raw_mode().unwrap();
-
-    write!(
-        stdout,
-        "{}{}Use the arrow keys to control the game.\r\n{}{}",
-        termion::clear::All,
-        termion::cursor::Goto(1, 1),
-        GameEngine::to_str(board),
-        termion::cursor::Hide
-    )
-    .unwrap();
-
-    stdout.flush().unwrap();
-
-    for c in stdin.keys() {
-        write!(
-            stdout,
-            "{}{}",
-            termion::cursor::Goto(1, 1),
-            termion::clear::All,
-        )
-        .unwrap();
-
-        match c.unwrap() {
-            Key::Ctrl('c') => break,
-            Key::Left => board = GameEngine::move_left(board),
-            Key::Right => board = GameEngine::move_right(board),
-            Key::Up => board = GameEngine::move_up(board),
-            Key::Down => board = GameEngine::move_down(board),
-            _ => (),
-        }
-
-        if GameEngine::is_game_over(board) {
-            break;
-        };
-
-        write!(stdout, "{}", GameEngine::to_str(board)).unwrap();
-        stdout.flush().unwrap();
-    }
-
-    write!(stdout, "{}", termion::cursor::Show).unwrap();
-}
+//use std::io::{stdin, stdout, Write};
+//use termion::event::Key;
+//use termion::input::TermRead;
+//use termion::raw::IntoRawMode;
+//
+//use crate::engine as GameEngine;
+//
+//pub fn start_game_in_ui() {
+//    let mut board = GameEngine::new_game();
+//    let stdin = stdin();
+//
+//    let mut stdout = stdout().into_raw_mode().unwrap();
+//
+//    write!(
+//        stdout,
+//        "{}{}Use the arrow keys to control the game.\r\n{}{}",
+//        termion::clear::All,
+//        termion::cursor::Goto(1, 1),
+//        GameEngine::to_str(board),
+//        termion::cursor::Hide
+//    )
+//    .unwrap();
+//
+//    stdout.flush().unwrap();
+//
+//    for c in stdin.keys() {
+//        write!(
+//            stdout,
+//            "{}{}",
+//            termion::cursor::Goto(1, 1),
+//            termion::clear::All,
+//        )
+//        .unwrap();
+//
+//        match c.unwrap() {
+//            Key::Ctrl('c') => break,
+//            Key::Left => board = GameEngine::move_left(board),
+//            Key::Right => board = GameEngine::move_right(board),
+//            Key::Up => board = GameEngine::move_up(board),
+//            Key::Down => board = GameEngine::move_down(board),
+//            _ => (),
+//        }
+//
+//        if GameEngine::is_game_over(board) {
+//            break;
+//        };
+//
+//        write!(stdout, "{}", GameEngine::to_str(board)).unwrap();
+//        stdout.flush().unwrap();
+//    }
+//
+//    write!(stdout, "{}", termion::cursor::Show).unwrap();
+//}
