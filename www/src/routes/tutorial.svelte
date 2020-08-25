4bb8f3a73585dda4cb5fe0f764f7c05fa309cc84
Matthew Kennedy <kennedymj97@gmail.com>
2020-08-25 10:28:54 +0100

    fix spelling error in the tutorial

--- www/src/routes/tutorial.svelte
+++ www/src/routes/tutorial.svelte
@@ -93,7 +93,7 @@
     {
       name: 'Try move down if creates monotonic middle top row',
       description:
-        'The middle top row is the one below the top row. Monotonicity means the values for the tiles in the row are decreasing/equal all the way across the row. A fully empty row does not count as being monotonic. It can be monotonic either left to right or right to left. If the middle top row is not currently monotonic and making the move down makes it monotonic, then try making the move down. An empty tile counts as 0. This rule often applys when there are tiles on the middle top row seperated by empty tiles, in this case the row is not monotonic. If after making the move down there are no longer tiles seperated by empty tiles you should try making the move down.',
+        'The middle top row is the one below the top row. Monotonicity means the values for the tiles in the row are decreasing/equal all the way across the row. A fully empty row does not count as being monotonic. It can be monotonic either left to right or right to left. If the middle top row is not currently monotonic and making the move down makes it monotonic, then try making the move down. An empty tile counts as 0. This rule often applies when there are tiles on the middle top row seperated by empty tiles, in this case the row is not monotonic. If after making the move down there are no longer tiles seperated by empty tiles you should try making the move down.',
       examples: [
         {
           imagePath: './tutorial_images/monotonic/already_monotonic.png',
